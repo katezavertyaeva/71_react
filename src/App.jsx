65b6408d945b3ce0895a0a8a829008b4f14e1abf
katezavertyaeva <katsiarynazavertyaeva@gmail.com>
2025-11-04 21:46:57 +0100

    add lesson5 fp

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@
 // Импорты лекций
 // import Lesson02 from "./lessons/Lesson02/Lesson02";
 // import Lesson03 from "./lessons/Lesson03/Lesson03";
-import Lesson04 from "./lessons/Lesson04/Lesson04";
-
+// import Lesson04 from "./lessons/Lesson04/Lesson04";
+import Lesson05 from "./lessons/Lesson05/Lesson05";
 
 // Импорты домашних работ
 // import Homework02 from "./homeworks/Homework02/Homework02";
 // import Homework03 from "./homeworks/Homework03/Homework03";
+// import Homework04 from "./homeworks/Homework04/Homework04";
 
 function App() {
   return (
@@ -29,7 +30,11 @@ function App() {
       {/* <Homework03 /> */}
 
       {/* Лекция 4 */}
-      <Lesson04 />
+      {/* <Lesson04 /> */}
+      {/* <Homework04 /> */}
+
+      {/* Лекция 5 */}
+      <Lesson05 />
     </>
   );
 }
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+import "./styles.css";
+import Button from "../Button/Button";
+
+function Feedback() {
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+
+  const addLike = () => {
+    setLikes((prevState) => prevState + 1);
+  };
+
+  const addDislike = () => {
+    setDislikes((prevState) => prevState + 1);
+  };
+
+  const resetResults = () => {
+    setLikes(0);
+    setDislikes(0);
+  };
+
+  return (
+    <div className="feedback-container">
+      <div className="feedback-results-container">
+        <div className="like-dislike-container">
+          <div className="result">{likes}</div>
+          <Button name="LIKE" onClick={addLike} />
+        </div>
+        <div className="like-dislike-container">
+          <div className="result">{dislikes}</div>
+          <Button name="DISLIKE" onClick={addDislike} />
+        </div>
+      </div>
+      <Button name="RESET RESULTS" onClick={resetResults} />
+    </div>
+  );
+}
+
+export default Feedback;
