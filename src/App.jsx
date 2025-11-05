c30defb938a98f8d74b065ee0dd5d1cc7c9fb95c
katezavertyaeva <katsiarynazavertyaeva@gmail.com>
2025-11-05 21:05:26 +0100

    add lesson6 fp

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@
 // import Lesson02 from "./lessons/Lesson02/Lesson02";
 // import Lesson03 from "./lessons/Lesson03/Lesson03";
 // import Lesson04 from "./lessons/Lesson04/Lesson04";
-import Lesson05 from "./lessons/Lesson05/Lesson05";
+// import Lesson05 from "./lessons/Lesson05/Lesson05";
 
 // Импорты домашних работ
 // import Homework02 from "./homeworks/Homework02/Homework02";
 // import Homework03 from "./homeworks/Homework03/Homework03";
 // import Homework04 from "./homeworks/Homework04/Homework04";
+import Homework05 from "./homeworks/Homework05/Homework05";
+
 
 function App() {
   return (
@@ -34,7 +36,8 @@ function App() {
       {/* <Homework04 /> */}
 
       {/* Лекция 5 */}
-      <Lesson05 />
+      {/* <Lesson05 /> */}
+      <Homework05 />
     </>
   );
 }
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+function Product({ productName, productPrice }) {
+  return (
+    <div className="product-wrapper">
+      <h3>{productName}</h3>
+      {/* Пример тернарного оператора для скрытия элемента*/}
+      {/* {productPrice !== undefined ? <div>{productPrice}$</div> : null} */}
+      {/* true ? <div>: null */}
+      {/* {productPrice ? <div>{productPrice}$</div> : null} */}
+      {/* Пример использования оператора условного рендеринга */}
+      {productPrice && <div>{productPrice}$</div>}
+    </div>
+  );
+}
+
+export default Product;
